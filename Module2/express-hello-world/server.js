a03d0ded344e87693b10ef368fb8fe8b400f9e19
JohanQuiroga <quirogacj@utp.edu.co>
2018-01-25 11:33:26 -0500

    Added all the main Express app sections

--- Module2/express-hello-world/server.js
+++ Module2/express-hello-world/server.js
@@ -2,9 +2,17 @@
 const express = require('express')
 // Instantiations
 const app = express()
+
+// Configurations
+
+// Middleware
+
 // Routes
 app.get('/', (req, res) => {
     res.send({msg: 'hello world'})
 })
+
+// Error handlers
+
 // Bootup
 app.listen(3000)
