35ba3ae20fec6eebf13b280faa87c40a165a8e4a
JohanQuiroga <quirogacj@utp.edu.co>
2018-01-25 13:17:11 -0500

    Added URL parameters and Query String Data to api routes

--- Module2/rest-api-routing/server.js
+++ Module2/rest-api-routing/server.js
@@ -5,34 +5,35 @@ const bodyParser = require('body-parser')
 // Instantiations
 const app = express()
 
-let profile = {
+let profile = [{
     username: 'azat',
     email: '[reducted]',
     url: 'http:azat.co'
-}
+}]
 
 // Middlewares
 app.use(bodyParser.json())
 
 // Routes
 app.get('/profile', (req, res) => {
+    if (req.query.id) return res.send(profile[req.query.id])
     res.send(profile)
 })
 
 app.post('/profile', (req, res) => {
-    profile = req.body
+    profile.push(req.body)
     console.log('created', profile)
     res.sendStatus(201)
 })
 
-app.put('/profile', (req, res) => {
-    Object.assign(profile, req.body)
-    console.log('updated', profile)
+app.put('/profile/:id', (req, res) => {
+    Object.assign(profile[req.params.id], req.body)
+    console.log('updated', profile[req.params.id])
     res.sendStatus(204)
 })
 
-app.delete('/profile', (req, res) => {
-    profile = {}
+app.delete('/profile/:id', (req, res) => {
+    profile.splice(req.params.id, 1)
     console.log('deleted', profile)
     res.sendStatus(204)
 })
