83e499a0fe7a995c3d708e5cb9e605bfc5ae0845
JohanQuiroga <quirogacj@utp.edu.co>
2018-01-27 15:53:25 -0500

    Introduction to Mongoose, connecting to a database and saving a document (a model instance)

---
+++ Module4/mongoose-project/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/test')
+
+let Book = mongoose.model('Book', { name: String })
+
+let practicalNodeBook = new Book({name: 'Practical Node.js, 2nd Edition'})
+
+practicalNodeBook.save((err, results) => {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    } else {
+        console.log('Saved:', results)
+        process.exit(0)
+    }
+})
